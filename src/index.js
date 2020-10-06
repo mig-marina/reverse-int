23d654eb7572c258f82468ce770770dea8bed3cc
Maryna Migacheva <m.migacheva@gmail.com>
2020-10-06 10:57:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let newN = n;
+  if(newN<0) {newN *= -1};
+  newN = String(newN).split('').reverse().join('');
+  newN = Number(newN);
+  return newN;
 }
